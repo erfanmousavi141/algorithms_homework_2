0cf043f28bb1c9eb6074de1a2cf74a50f2a2c22e
erfanmousavi141 <149951022+erfanmousavi141@users.noreply.github.com>
2023-11-18 18:54:28 +0330

    Update tree_recursive.py

--- tree_recursive.py
+++ tree_recursive.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 
-a = [8, 3, 7, 12, 18, 1, 53, 73, 14, 13, 10, 6]
-
-# [Value, Parent, LeftChild, Index, RightChild]
-b = [[a[index], None, None, index, None] for index in range(len(a))]
-
+array = [8, 3, 7, 12, 18, 1, 53, 73, 14, 13, 10, 6]
+#    [LeftChild, Index, RightChild]
+result = [[None, index, None] for index in range(len(array))]  # initializing the result list
 
 def make_tree(lst: list) -> None:
-    global b
+    global result
     rootValue = lst[0]  # consider the first node of the 'sequence' as the root
-    rootIndex = a.index(rootValue)
-    root_in_b = b[rootIndex]
+    rootIndex = array.index(rootValue)
+    root_in_b = result[rootIndex]
     root_lefts = []
     root_rights = []
 
@@ -21,49 +19,41 @@ def make_tree(lst: list) -> None:
         root_lefts.append(node)  # or it's left of root
 
     if len(root_rights) >= 2 and len(root_lefts) >= 2:
-        lChildIndex = a.index(root_lefts[0])
-        rChildIndex = a.index(root_rights[0])
-        root_in_b[4] = rChildIndex  # set rChild of this node(root)
-        root_in_b[2] = lChildIndex  # set lChild of this node(root)
-        b[rChildIndex][1] = rootIndex  # set parent of this rChild
-        b[lChildIndex][1] = rootIndex  # set parent of this lChild
+        lChildIndex = array.index(root_lefts[0])
+        rChildIndex = array.index(root_rights[0])
+        root_in_b[2] = rChildIndex  # set rChild of this node(root)
+        root_in_b[0] = lChildIndex  # set lChild of this node(root)
         make_tree(root_rights)  # do this algorithm for root_rights
         make_tree(root_lefts)  # do this algorithm for root_lefts
 
     elif len(root_rights) >= 2 and len(root_lefts) < 2:
-        rChildIndex = a.index(root_rights[0])
-        root_in_b[4] = rChildIndex  # set rChild of this node(root)
-        b[rChildIndex][1] = rootIndex  # set parent of this rChild
+        rChildIndex = array.index(root_rights[0])
+        root_in_b[2] = rChildIndex  # set rChild of this node(root)
         if len(root_lefts) != 0:  # if the root_lefts list has 1 node in it:
-            lChildIndex = a.index(root_lefts[0])
-            root_in_b[2] = lChildIndex  # set lChild of this node(root)
-            b[lChildIndex][1] = rootIndex  # set parent of this lChild
+            lChildIndex = array.index(root_lefts[0])
+            root_in_b[0] = lChildIndex  # set lChild of this node(root)
         make_tree(root_rights)  # do this algorithm for root_rights
 
     elif len(root_rights) < 2 and len(root_lefts) >= 2:
-        lChildIndex = a.index(root_lefts[0])
-        root_in_b[2] = lChildIndex  # set lChild of this node(root)
-        b[lChildIndex][1] = rootIndex  # set parent of this lChild
+        lChildIndex = array.index(root_lefts[0])
+        root_in_b[0] = lChildIndex  # set lChild of this node(root)
         if len(root_rights) != 0:  # if the root_rights list has 1 node in it:
-            rChildIndex = a.index(root_rights[0])
-            root_in_b[4] = rChildIndex  # set rChild of this node(root)
-            b[rChildIndex][1] = rootIndex  # set parent of this rChild
+            rChildIndex = array.index(root_rights[0])
+            root_in_b[2] = rChildIndex  # set rChild of this node(root)
         make_tree(root_lefts)  # do this algorithm for root_lefts
 
     elif len(root_rights) < 2 and len(root_lefts) < 2:
         if len(root_rights) != 0:  # if the root_rights list has 1 node in it:
-            rChildIndex = a.index(root_rights[0])
-            root_in_b[4] = rChildIndex  # set rChild of this node(root)
-            b[rChildIndex][1] = rootIndex  # set parent of this rChild
+            rChildIndex = array.index(root_rights[0])
+            root_in_b[2] = rChildIndex  # set rChild of this node(root)
         if len(root_lefts) != 0:  # if the root_lefts list has 1 node in it:
-            lChildIndex = a.index(root_lefts[0])
-            root_in_b[2] = lChildIndex  # set lChild of this node(root)
-            b[lChildIndex][1] = rootIndex  # set parent of this lChild
+            lChildIndex = array.index(root_lefts[0])
+            root_in_b[0] = lChildIndex  # set lChild of this node(root)
 
 
-make_tree(a)
+make_tree(array)
 
 # print the result
-print("[Value, Parent, LeftChild, Index, RightChild]")
-for i in b:
-    print(i)
+print("[LeftChild, Index, RightChild]")
+for row in result:
+    print(row)
